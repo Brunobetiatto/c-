d863dd1ff1cf765d6f3d49e63d02f4bb9ea10f68
Bruno Betiatto Alves <127573399+Brunobetiatto@users.noreply.github.com>
2026-05-11 16:41:51 -0300

    adição de algumas rotas

--- MeuPrimeiroBackend/Program.cs
+++ MeuPrimeiroBackend/Program.cs
@@ -1,10 +1,21 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Avisamos o projeto que vamos usar o banco de dados
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // URL do Angular
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 builder.Services.AddDbContext<AppDbContext>();
 
+
 var app = builder.Build();
+
+
+app.UseCors("PermitirAngular");
 
 // ROTA PARA BUSCAR: Vai no banco e traz todos os usuários
 app.MapGet("/usuarios", (AppDbContext banco) => 
@@ -22,13 +33,14 @@ app.MapPost("/usuarios", (Usuario novoUsuario, AppDbContext banco) =>
     
 });
 
-app.MapPost("/usuarios/{id}", (int id, Usuario usuarioAtualizado, AppDbContext banco) => 
+app.MapPost("/usuarios/{id:int}", (int id, Usuario usuarioAtualizado, AppDbContext banco) => 
 {
     var usuario = banco.Usuarios.Find(id);
     if (usuario == null)
     {
         return Results.NotFound();
-    }
+    } 
+    
 
     usuario.Nome = usuarioAtualizado.Nome;
     usuario.Email = usuarioAtualizado.Email;
@@ -36,6 +48,30 @@ app.MapPost("/usuarios/{id}", (int id, Usuario usuarioAtualizado, AppDbContext b
 
     banco.SaveChanges();
     return Results.Ok($"Usuário {usuario.Nome} atualizado com sucesso!");
+});
+
+app.MapGet("/usuarios/{id:int}", (int id, AppDbContext banco) => 
+{
+    var usuario = banco.Usuarios.Find(id);
+    if (usuario == null)
+    {
+        return Results.NotFound();
+    } 
+    return Results.Ok(usuario);
+});
+
+app.MapGet("/usuarios/abacate", (AppDbContext banco) => 
+{
+    var usuariosComAbacate = banco.Usuarios.Where(u => u.Abacate).ToList();
+    return Results.Ok(usuariosComAbacate);
+});
+
+app.MapDelete("/usuarios/deleteAll", (AppDbContext banco) => 
+{
+    var todosUsuarios = banco.Usuarios.ToList();
+    banco.Usuarios.RemoveRange(todosUsuarios);
+    banco.SaveChanges();
+    return Results.Ok("Todos os usuários foram deletados.");
 });
 
 app.Run();
